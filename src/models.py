a17499b399b0c6a349b5137da569f52abc17db3a
Manuel García <153007701+delafter@users.noreply.github.com>
2024-04-22 19:32:34 +0000

    fin

--- src/models.py
+++ src/models.py
@@ -7,23 +7,53 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Usuario(Base):
+    __tablename__ = 'usuario'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    usuario_id = Column(Integer, ForeignKey('favoritos.usuario_id'))
 
-class Address(Base):
-    __tablename__ = 'address'
+class Planets(Base):
+    __tablename__ = 'planets'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    climate = Column(String(250))
+    diameter = Column(Integer)
+    terrain = Column(String(250), nullable=False)
+   
+    
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    manufacturer = Column(String(250))
+    model = Column(String(250))
+    hyperdrive_rating = Column(Integer)
+   
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    mass = Column(Integer)
+    hair_color = Column(String(250))
+    birth_year = Column(String(250))
+   
+
+class favoritos(Base):
+    __tablename__ = 'favoritos'
+   
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('usuario.id'))
+
+
+
+    
+
+
+  
+
 
     def to_dict(self):
         return {}
